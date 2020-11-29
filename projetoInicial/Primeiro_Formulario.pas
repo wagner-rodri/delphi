451a3418643e615a659299961ce3eedd40430111
wagner-rodri <luan.frra@gmail.com>
2020-11-28 22:43:16 -0300

    Indented code

--- projetoInicial/Primeiro_Formulario.pas
+++ projetoInicial/Primeiro_Formulario.pas
@@ -48,75 +48,75 @@ implementation
 {$R *.dfm}
 
 procedure TCad_User.btn_calcularClick(Sender: TObject);
-var vlr1, vlr2, total, media:double;
-var cont: integer;
-begin
-  vlr1 := StrToFloat(txt_vlr_1.Text);
-  vlr2 := StrToFloat(txt_vlr_2.Text);
-
-  total := vlr1 + vlr2;
-  txt_vlr_total.Text := FloatToStr(total);
-
-  media := 60;
-  if (total >= media) AND (total > 80) Then
+  var vlr1, vlr2, total, media:double;
+  var cont: integer;
     begin
-      ShowMessage('O aluno obteve média de ' + FloatToStr(total) + ', portanto está aprovado!');
-    end
-  Else
-    begin
-      ShowMessage('O aluno obteve média de ' + FloatToStr(total) + ', portanto está Reprovado! =(');
-    end;
+      vlr1 := StrToFloat(txt_vlr_1.Text);
+      vlr2 := StrToFloat(txt_vlr_2.Text);
 
-  {for cont := 5 DownTo 0 do
-    begin
-      ShowMessage(IntToStr(cont));
-      if cont <= 0 then
+      total := vlr1 + vlr2;
+      txt_vlr_total.Text := FloatToStr(total);
+
+      media := 60;
+      if (total >= media) AND (total > 80) Then
         begin
-          ShowMessage('o contador chegou a 0')
+          ShowMessage('O aluno obteve média de ' + FloatToStr(total) + ', portanto está aprovado!');
+        end
+      Else
+        begin
+          ShowMessage('O aluno obteve média de ' + FloatToStr(total) + ', portanto está Reprovado! =(');
         end;
-    end;}
 
-  cont := 1;
-  while cont <= 5 do
-    begin
-      ShowMessage(IntToStr(cont));
-      inc(cont);
-    end;
+      {for cont := 5 DownTo 0 do
+        begin
+          ShowMessage(IntToStr(cont));
+          if cont <= 0 then
+            begin
+              ShowMessage('o contador chegou a 0')
+            end;
+        end;}
 
-end;
+      cont := 1;
+      while cont <= 5 do
+        begin
+          ShowMessage(IntToStr(cont));
+          inc(cont);
+        end;
+
+  end;
 
 procedure TCad_User.btn_deletarClick(Sender: TObject);
-begin
-  nome := 'Deu Certo!';
-  sobrenome := 'Parabéns!';
-  endereco := 'Rua B';
-  txt_nome.Text := nome;
-  txt_nome2.Text := sobrenome;
-  txt_nome3.Text := endereco;
-end;
+  begin
+    nome := 'Deu Certo!';
+    sobrenome := 'Parabéns!';
+    endereco := 'Rua B';
+    txt_nome.Text := nome;
+    txt_nome2.Text := sobrenome;
+    txt_nome3.Text := endereco;
+  end;
 
 procedure TCad_User.btn_editarClick(Sender: TObject);
-begin
-  nome := 'Deu Certo!';
-  sobrenome := 'Parabéns!';
-  endereco := 'Rua B';
-  lb_valor.Caption := nome + ' ' + sobrenome + ' ' + endereco;
-end;
+  begin
+    nome := 'Deu Certo!';
+    sobrenome := 'Parabéns!';
+    endereco := 'Rua B';
+    lb_valor.Caption := nome + ' ' + sobrenome + ' ' + endereco;
+  end;
 
 procedure TCad_User.btn_icone_salvarClick(Sender: TObject);
-begin
-  valor := 5;
-  lb_valor.Caption := IntToStr(valor);
-end;
+  begin
+    valor := 5;
+    lb_valor.Caption := IntToStr(valor);
+  end;
 
 procedure TCad_User.btn_salvarClick(Sender: TObject);
-begin
-  nome := 'Wagner';
-  sobrenome := 'Rodrigues';
-  endereco := 'Rua A';
-  txt_nome.Text := nome;
-  txt_nome2.Text := sobrenome;
-  txt_nome3.Text := endereco;
-end;
+  begin
+    nome := 'Wagner';
+    sobrenome := 'Rodrigues';
+    endereco := 'Rua A';
+    txt_nome.Text := nome;
+    txt_nome2.Text := sobrenome;
+    txt_nome3.Text := endereco;
+  end;
 
 end.
