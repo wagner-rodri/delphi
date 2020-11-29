b3a6d7a68afd83d19942af55686b95ddae5880a8
wagner-rodri <luan.frra@gmail.com>
2020-11-29 20:59:10 -0300

    Created Calculator Project
    Added Buttons

--- calculadora/U_Calculadora.pas
+++ calculadora/U_Calculadora.pas
@@ -5,20 +5,27 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
 
 type
   TForm1 = class(TForm)
+    Layout1: TLayout;
+    btn_igual: TButton;
+    btn_1: TButton;
+    btn_ponto: TButton;
+    btn_2: TButton;
+    btn_multi: TButton;
     btn_0: TButton;
     btn_7: TButton;
-    btn_6: TButton;
-    btn_4: TButton;
-    btn_5: TButton;
-    btn_2: TButton;
-    btn_3: TButton;
-    Button6: TButton;
     btn_8: TButton;
     btn_9: TButton;
+    btn_6: TButton;
+    btn_5: TButton;
+    btn_somar: TButton;
+    btn_sub: TButton;
+    btn_3: TButton;
+    btn_div: TButton;
+    btn_4: TButton;
   private
     { Private declarations }
   public
