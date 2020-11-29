eccc9dc0250978cd62c05bb63d370f9951075415
wagner-rodri <luan.frra@gmail.com>
2020-11-29 20:31:11 -0300

    Created Calculator Project

---
+++ calculadora/U_Calculadora.pas
@@ -0,0 +1,35 @@
+unit U_Calculadora;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    btn_0: TButton;
+    btn_7: TButton;
+    btn_6: TButton;
+    btn_4: TButton;
+    btn_5: TButton;
+    btn_2: TButton;
+    btn_3: TButton;
+    Button6: TButton;
+    btn_8: TButton;
+    btn_9: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
