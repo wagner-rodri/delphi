52d98fa6aae9be8e0c89e4d597de1a1341444a1a
wagner-rodri <luan.frra@gmail.com>
2020-11-29 21:18:24 -0300

    Enabled and disable math functions buttons

--- calculadora/U_Calculadora.pas
+++ calculadora/U_Calculadora.pas
@@ -5,11 +5,11 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Edit;
 
 type
   TForm1 = class(TForm)
-    Layout1: TLayout;
+    caixa_botoes: TLayout;
     btn_igual: TButton;
     btn_1: TButton;
     btn_ponto: TButton;
@@ -26,6 +26,15 @@ type
     btn_3: TButton;
     btn_div: TButton;
     btn_4: TButton;
+    caixa_topo: TLayout;
+    lb_operador: TLabel;
+    txt_num_1: TEdit;
+    txt_num_2: TEdit;
+    procedure btn_subClick(Sender: TObject);
+    procedure btn_somarClick(Sender: TObject);
+    procedure btn_divClick(Sender: TObject);
+    procedure btn_multiClick(Sender: TObject);
+    procedure btn_igualClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -38,5 +47,48 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.btn_divClick(Sender: TObject);
+begin
+  lb_operador.Text := '/';
+  btn_somar.Enabled := true;
+  btn_sub.Enabled := true;
+  btn_multi.Enabled := true;
+  btn_div.Enabled := false;
+end;
+
+procedure TForm1.btn_igualClick(Sender: TObject);
+  var n1, n2 : double;
+begin
+  n1 := StrToFloat(txt_num_1.Text);
+  n2 := StrToFloat(txt_num_2.Text);
+end;
+
+procedure TForm1.btn_multiClick(Sender: TObject);
+  begin
+    lb_operador.Text := 'x';
+    btn_somar.Enabled := true;
+    btn_sub.Enabled := true;
+    btn_multi.Enabled := false;
+    btn_div.Enabled := true;
+  end;
+
+procedure TForm1.btn_somarClick(Sender: TObject);
+  begin
+    lb_operador.Text := '+';
+    btn_somar.Enabled := false;
+    btn_sub.Enabled := true;
+    btn_multi.Enabled := true;
+    btn_div.Enabled := true;
+  end;
+
+procedure TForm1.btn_subClick(Sender: TObject);
+  begin
+    lb_operador.Text := '-';
+    btn_somar.Enabled := true;
+    btn_sub.Enabled := false;
+    btn_multi.Enabled := true;
+    btn_div.Enabled := true;
+  end;
 
 end.
