37deac11f9ba8fae669f94cf1e6ced73555922d6
wagner-rodri <luan.frra@gmail.com>
2020-11-30 20:49:37 -0300

    Created Timer App

---
+++ timer/U_Cronometro.pas
@@ -0,0 +1,48 @@
+unit U_Cronometro;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Buttons, Vcl.MPlayer, Vcl.StdCtrls,
+  Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    t_timer: TTimer;
+    lb_dois_pontos: TLabel;
+    lb_minutos: TLabel;
+    MediaPlayer1: TMediaPlayer;
+    btn_play: TSpeedButton;
+    lb_segundos: TLabel;
+    procedure t_timerTimer(Sender: TObject);
+    procedure btn_playClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btn_playClick(Sender: TObject);
+begin
+ t_timer.Enabled := true;
+end;
+
+procedure TForm1.t_timerTimer(Sender: TObject);
+  var seg, min, cont : Integer;
+begin
+  seg := strToInt(lb_segundos.Caption);
+  min := strToInt(lb_minutos.Caption);
+  cont := seg + 1;
+
+  lb_segundos.Caption := intToStr(cont);
+end;
+
+end.
